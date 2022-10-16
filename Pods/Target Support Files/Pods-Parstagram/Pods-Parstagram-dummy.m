54f4fadf958e5f8fbb060a2ceba4f3e539bcf960
Guilherme Camara <guiadebola2@gmail.com>
2022-10-15 23:53:53 -0400

    fixing

--- Pods/Target Support Files/Pods-Parstagram/Pods-Parstagram-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ParstaGram : NSObject
-@end
-@implementation PodsDummy_Pods_ParstaGram
-@end
--- Pods/Target Support Files/Pods-ParstagramTests/Pods-ParstagramTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ParstaGramTests : NSObject
-@end
-@implementation PodsDummy_Pods_ParstaGramTests
-@end
